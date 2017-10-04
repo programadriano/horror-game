97f5346e1b18b800dbe664b9a70d671a3416a88d
Thiago Adriano <tadriano.net@gmail.com>
2017-10-03 22:08:11 -0300

    add game

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+
+    public static int life { get; set; }
+    private int initialLife { get; set; }
+
+    // Use this for initialization
+    void Start()
+    {
+        initialLife = 100;
+        life = initialLife;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/flashlight.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flashlight : MonoBehaviour
+{
+    public static float batery;
+    public int maxBatery = 300;
+    private bool TurnOn;
+    // Use this for initialization
+    void Start()
+    {
+        batery = maxBatery;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Light>().intensity = 0.1f + 2 * batery / maxBatery;
+
+        if (Input.GetKeyDown(KeyCode.F) && TurnOn)
+        {
+            GetComponent<Light>().enabled = !GetComponent<Light>().enabled;
+        }
+
+        if (GetComponent<Light>().enabled)
+        {
+            batery -= Time.deltaTime;           
+        }
+
+        if (batery <= 0)
+        {
+            TurnOn = false;
+            GetComponent<Light>().enabled = false;
+        }
+        else
+        {
+            TurnOn = true;
+        }
+    }
+}
